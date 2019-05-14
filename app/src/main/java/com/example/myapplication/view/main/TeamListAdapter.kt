111ac3bde9ff64044903611392bd61503bb7b911
ho8278 <ho8278@naver.com>
2019-05-14 17:24:08 +0900

    현재 선택된 그룹 보여주기

--- app/src/main/java/com/example/myapplication/view/main/TeamListAdapter.kt
+++ app/src/main/java/com/example/myapplication/view/main/TeamListAdapter.kt
@@ -2,41 +2,94 @@ package com.example.myapplication.view.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.data.local.pref.PreferenceHelperImpl
 import com.example.myapplication.data.model.Team
+import com.example.myapplication.databinding.ItemAddGroupBinding
 import com.example.myapplication.databinding.ItemTeamBinding
+import com.example.myapplication.view.base.BaseViewHolder
 import java.util.*
 
-class TeamListAdapter(val listener:GroupChangeListener):RecyclerView.Adapter<TeamListAdapter.TeamViewHolder>(){
+class TeamListAdapter(val listener: GroupChangeListener) : RecyclerView.Adapter<BaseViewHolder>() {
 
-    val groupList= mutableListOf<Team>()
+    private val ADD_VIEWHOLDER = 1
+    private val TEAM_VIEWHOLDER = 2
+    private var isCurrent = false
 
-    fun setList(list:MutableList<Team>){
+    val groupList = mutableListOf<Team>()
+
+    fun setList(list: MutableList<Team>) {
         groupList.clear()
         groupList.addAll(list)
+        groupList.add(Team("dummy", "dummy"))
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamViewHolder {
-        val binding=DataBindingUtil.inflate<ItemTeamBinding>(LayoutInflater.from(parent.context), R.layout.item_team,parent,false)
-        return TeamViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        if (viewType == ADD_VIEWHOLDER) {
+            val binding = DataBindingUtil.inflate<ItemAddGroupBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.item_add_group,
+                parent,
+                false
+            )
+            return AddTeamViewHolder(binding)
+        } else {
+            val binding = DataBindingUtil.inflate<ItemTeamBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.item_team,
+                parent,
+                false
+            )
+            return TeamViewHolder(binding)
+        }
     }
 
     override fun getItemCount(): Int {
         return groupList.size
     }
 
-    override fun onBindViewHolder(holder: TeamViewHolder, position: Int) {
+    override fun getItemViewType(position: Int): Int {
+        if (position == groupList.lastIndex)
+            return ADD_VIEWHOLDER
+        else
+            return TEAM_VIEWHOLDER
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         holder.bind(position)
     }
 
-    inner class TeamViewHolder(val binding: ItemTeamBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(position:Int){
-            binding.viewmodel=TeamViewModel(AppInitialize.dataSource,groupList[position])
+    inner class TeamViewHolder(val binding: ItemTeamBinding) : BaseViewHolder(binding) {
+
+        override fun bind(position: Int) {
+            binding.viewmodel = TeamViewModel(AppInitialize.dataSource, groupList[position])
+            val currentGroupID = AppInitialize.dataSource.getItem<String>(PreferenceHelperImpl.CURRENT_GROUP_ID)
+            if (currentGroupID.isEmpty() && isCurrent == false) {
+                binding.llContainer.setBackgroundColor(
+                    ContextCompat.getColor(
+                        binding.llContainer.context,
+                        R.color.colorCurrentGroup
+                    )
+                )
+                isCurrent = true
+            } else if (currentGroupID == groupList[position].id && isCurrent == false) {
+                binding.llContainer.setOnClickListener {
+                    listener.change(groupList[position].id)
+                }
+                isCurrent = true
+            }
+        }
+    }
+
+    inner class AddTeamViewHolder(val binding: ItemAddGroupBinding) : BaseViewHolder(binding) {
+        override fun bind(position: Int) {
             binding.llContainer.setOnClickListener {
-                listener.change(groupList[position].id)
+                //TODO:그룹 생성 액티비티 추가
             }
         }
     }
